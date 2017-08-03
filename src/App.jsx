cf7d1724c1d843fc1369ff606a49a676abb3870c
Erik Lopez <erik@eriklopez.work>
2017-08-02 22:26:35 -0500

    Adds create APi to createIssue method

--- src/App.jsx
+++ src/App.jsx
@@ -87,7 +87,7 @@ class IssueList extends React.Component {
     loadData() {
         fetch('/api/issues/').then(response => 
         response.json()
-    ).then(data => {
+        ).then(data => {
         console.log("Total count of records: ", data._metadata.totalcount);
         data.records.forEach(issue => {
             issue.created = new Date(issue.created);
@@ -95,9 +95,9 @@ class IssueList extends React.Component {
                 issue.completionDate = new Date(issue.completionDate);
         });
         this.setState({issues: data.records});
-    }).catch(err => {
-        console.log(err);
-    });
+        }).catch(err => {
+            console.log(err);
+        });
     }
 
     componentDidMount() {
@@ -105,10 +105,20 @@ class IssueList extends React.Component {
     }
 
     createIssue(newIssue) {
-        const newIssues = this.state.issues.slice();
-        newIssue.id = this.state.issues.length + 1;
-        newIssues.push(newIssue);
-        this.setState({issues: newIssues});
+        fetch('/api/issues', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(newIssue),  
+        }).then(response => response.json()
+        ).then(updatedIssue => {
+            updatedIssue.created = new Date(updatedIssue.created);
+            if (updatedIssue.completionDate)
+                updatedIssue.completionDate = new Date(updatedIssue.completionDate);
+            const newIssues = this.state.issues.concat(updatedIssue);
+            this.setState({issues: newIssues});
+        }).catch(err => {
+            alert("Error in sending data to server: " + err.message);
+        });
     }
 
     render() {
