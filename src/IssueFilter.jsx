76be91c2d0e52da94f1cb482f8d5573f4869af36
Erik Lopez <erik@eriklopez.work>
2017-09-16 22:04:44 -0500

    Adds Route query strings to client side

--- src/IssueFilter.jsx
+++ src/IssueFilter.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class IssueFilter extends React.Component { // eslint-disable-line
   render() {
+    const Seperator = () => <span> | </span>;
     return (
-      <div>Placeholder for the issue filter</div>
+      <div>
+        <Link to="/issues">All Issues</Link>
+        <Seperator />
+        <Link to={{ pathname: '/issues', query: {status : 'Open'}}}>Open Issues</Link>
+        <Seperator />
+        <Link to="/issues?status=Assigned">Assigned Issues</Link>
+      </div>
     );
   }
 }
