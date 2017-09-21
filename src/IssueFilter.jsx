2f99a2ef5e1bc4e14230cdbeaaf47ed0c4edf8cc
Erik <erik@eriklopez.work>
2017-09-20 22:28:19 -0500

    Fixes linter errors

--- src/IssueFilter.jsx
+++ src/IssueFilter.jsx
@@ -8,7 +8,7 @@ export default class IssueFilter extends React.Component { // eslint-disable-lin
       <div>
         <Link to="/issues">All Issues</Link>
         <Seperator />
-        <Link to={{ pathname: '/issues', query: {status : 'Open'}}}>Open Issues</Link>
+        <Link to={{ pathname: '/issues', query: { status: 'Open' } }}>Open Issues</Link>
         <Seperator />
         <Link to="/issues?status=Assigned">Assigned Issues</Link>
       </div>
