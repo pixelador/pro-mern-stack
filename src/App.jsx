37e31e29240cb27352b8801f904883122458f667
Erik Lopez <erik@eriklopez.work>
2017-09-24 11:16:54 -0500

    Adds decorator component, browserHistory

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Redirect, hashHistory, withRouter } from 'react-router';
+import { Router, Route, Redirect, browserHistory, withRouter } from 'react-router';
 
 import IssueList from './IssueList.jsx';
 import IssueEdit from './IssueEdit.jsx';
@@ -9,12 +9,32 @@ import IssueEdit from './IssueEdit.jsx';
 const contentNode = document.getElementById('contents');
 const NoMatch = () => <p>Page Not Found</p>;
 
+const App = (props) => (
+  <div>
+    <div className="header">
+      <h1>Issue Tracker&trade;</h1>
+    </div>
+    <div className="contents">
+      {props.children}
+    </div>
+    <div className="footer">
+      Full source code availble at this <a href="https://github.com/pixelador/pro-mern-stack">GitHub Repository</a>.
+    </div>
+  </div>
+);
+
+App.propTypes = {
+  children: React.PropTypes.object.isRequired,
+};
+
 const RoutedApp = () => (
-  <Router history={hashHistory} >
+  <Router history={browserHistory} >
     <Redirect from="/" to="/issues" />
-    <Route path="/issues" component={withRouter(IssueList)} />
-    <Route path="/issues/:id" component={IssueEdit} />
-    <Route path="*" component={NoMatch} />
+    <Route path="/" component={App} >
+      <Route path="/issues" component={withRouter(IssueList)} />
+      <Route path="/issues/:id" component={IssueEdit} />
+      <Route path="*" component={NoMatch} />
+    </Route>
   </Router>
 );
 
